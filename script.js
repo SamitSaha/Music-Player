b96bd703c56d12e5bfdb81056852a58735affe9b
SamitSaha <samitsaha99@gmail.com>
2024-03-12 04:38:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@
 // The join() method is used to concatenate all the elements of an array into a single string. It takes an optional parameter called a separator which is used to separate each element of the array.
 // Optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined.
 // The find() method retrieves the first element within an array that fulfills the conditions specified in the provided callback function. If no element satisfies the condition, the method returns undefined.
-
+// The forEach method is used to loop through an array and perform a function on each element of the array. For example, suppose you have an array of numbers and you want to log each number to the console.
+// Use the filter() method to remove the song object that matches the id parameter from the userData?.songs array. The filter method keeps only the elements of an array that satisfy the callback function passed to it.
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
